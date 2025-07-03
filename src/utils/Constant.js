c274b2ea38cfbd2b52d1ff686b9df42cf3137610
Deepak Kumar <deepakroy7393@gmail.com>
2025-07-03 21:54:02 +0530

    remove extra slace

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,3 +1,3 @@
 
 // export const BASE_URL = "http://localhost:5001"
-export const BASE_URL = "https://dev-tinder-backend-lvou.onrender.com/"
+export const BASE_URL = "https://dev-tinder-backend-lvou.onrender.com"
